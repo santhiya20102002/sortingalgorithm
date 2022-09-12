6d8f381577f44196d0ae472c2aae2004f2a876a8
santhiya20102002 <107112191+santhiya20102002@users.noreply.github.com>
2022-09-12 21:53:16 +0530

    Add files via upload

---
+++ Insertionsort.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+
+
+public class Insertionsort
+ {
+    
+public static void main(String args[])
+    
+{
+      
+ Scanner sc=new Scanner(System.in);
+       
+int n=sc.nextInt();
+        
+int temp,j;
+        
+int a[]=new int[n];
+        
+for(int i=0;i<n;i++)
+        
+{
+            
+a[i]=sc.nextInt();
+        
+}
+   
+        
+for(int i=1;i<n;i++)
+        
+{
+            
+temp=a[i];
+            
+j=i-1;
+            
+while(j>=0&&a[j]>temp)
+                
+                
+{
+             
+                    
+a[j+1]=a[j];
+                    
+j--;
+                
+            
+}
+            
+a[j+1]=temp;
+        
+}
+        
+for(int i=0;i<n;i++)
+        
+{
+            
+System.out.print(a[i]+" ");
+        
+}
+
+
+    
+}
+   
+ 
+}
